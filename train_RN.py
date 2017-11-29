174e5d6a9f62fffc861ca4221a2dae99ca66dad0
chickenbestlover <jmpark@rit.kaist.ac.kr>
2017-11-29 19:00:16 +0900

    .

--- train_RN.py
+++ train_RN.py
@@ -61,8 +61,8 @@ parser.add_argument('--rnn_padding', action='store_true',
                     help='perform rnn padding (much slower but more accurate).')
 # model
 parser.add_argument('--question_merge', default='self_attn')
-parser.add_argument('--doc_layers', type=int, default=10)
-parser.add_argument('--question_layers', type=int, default=10)
+parser.add_argument('--doc_layers', type=int, default=5)
+parser.add_argument('--question_layers', type=int, default=5)
 parser.add_argument('--hidden_size', type=int, default=128)
 parser.add_argument('--num_features', type=int, default=4)
 parser.add_argument('--pos', type=str2bool, nargs='?', const=True, default=True,
@@ -87,7 +87,7 @@ parser.add_argument('--dropout_rnn_output', type=str2bool, nargs='?',
 parser.add_argument('--max_len', type=int, default=15)
 parser.add_argument('--rnn_type', default='lstm',
                     help='supported types: rnn, gru, lstm')
-parser.add_argument('--num_objects', type=int, default=4,
+parser.add_argument('--num_objects', type=int, default=10,
                     help='The number of objects needed for relationNet.')
 parser.add_argument('--reduction_ratio', type=int, default=2,
                     help='reduction_ratio')
